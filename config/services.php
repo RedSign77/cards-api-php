dd2019456c5dd9f31838496be0379b2dc101fada
Zoltán Németh <signred@gmail.com>
2025-10-11 18:47:40 +0200

    Remove Mailgun support Add Mailjet support

--- config/services.php
+++ config/services.php
@@ -35,11 +35,9 @@ return [
         ],
     ],
 
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-        'scheme' => 'https',
+    'mailjet' => [
+        'key' => env('MAILJET_APIKEY'),
+        'secret' => env('MAILJET_APISECRET'),
     ],
 
 ];
