14ed57601f7cf4681d4c275ac00eb519c11fa509
Zoltán Németh <signred@gmail.com>
2025-10-13 11:09:04 +0200

    Add approve email

---
+++ app/Notifications/UserApproved.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Webtech-solutions 2025, All rights reserved.
+ */
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserApproved extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct(
+        public User $approvedBy
+    ) {}
+
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Your Account Has Been Approved - ' . config('app.name'))
+            ->greeting('Hello ' . $notifiable->name . '!')
+            ->line('Great news! Your account has been approved by a supervisor.')
+            ->line('You now have full access to the platform and can start using all features.')
+            ->action('Login to Your Account', url('/admin/login'))
+            ->line('If you have any questions, please don\'t hesitate to contact us.')
+            ->line('Welcome aboard!');
+    }
+}
